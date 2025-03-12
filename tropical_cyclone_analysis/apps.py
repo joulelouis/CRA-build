ccba357507d9bfc5fc4287751ae88865004ebb0c
joulebuencido <joule.louis.buencido@gmail.com>
2025-03-13 06:10:54 +0800

    feat: added tropical cyclone analysis app

---
+++ tropical_cyclone_analysis/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class TropicalCycloneAnalysisConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'tropical_cyclone_analysis'
